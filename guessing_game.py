1011527f3ed1b5fb9563f0a4d7f48ca29cd47325
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-22 13:04:33 -0400

    hfh
    fgd

--- guessing_game.py
+++ guessing_game.py
@@ -2,9 +2,9 @@ def guessing_game():
     target = 15
     guess = int(input("Enter your guess: "))
     while(guess!=target):
-        guess = int(input("Enter your guess: "))
         if guess>target:
             print("Too high! Try again.")
         if guess<target:
             print("Too low! Try again.")
-    return print("Congratulations! You guessed it!")
+        guess = int(input("Enter your guess: "))
+    return "Congratulations! You guessed it!"
