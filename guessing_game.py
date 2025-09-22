95f948e6de3ef3c44a4941fa57440cd70d2f7cf5
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-22 13:00:29 -0400

    Update guessing_game.py

--- guessing_game.py
+++ guessing_game.py
@@ -1,12 +1,10 @@
 def guessing_game():
-    # TODO create guessing game function
-    # HINT: Target number should be 15
-    # HINT: Return message should be "Congratulations! You guessed it!"
-    # HINT: Use input("Enter your guess: ") for user input
-    # HINT: Print "Too low! Try again." for low guesses
-    # HINT: Print "Too high! Try again." for high guesses
-    pass
-
-if __name__ == "__main__":
-    # create guessing game below this
-    pass
+    target = 15
+    guess = int(input("Enter your guess: "))
+    while(guess!=target):
+        guess = int(input("Enter your guess: "))
+        if guess>target:
+            print("Too high! Try again.")
+        if guess<target:
+            print("Too low! Try again.")
+    return print("Congratulations! You guessed it!")
