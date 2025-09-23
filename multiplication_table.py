c0b0ec78af7067ed856ab76603b2a2b98138b874
Jackson Lam <03jackson.lam@kamsc.org>
2025-09-23 12:31:08 -0400

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -2,7 +2,7 @@ def multiplication_table(n):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             result = i * j
-            print(str(i)+" + "+str(j)+" = "+str(result))
+            print(str(i)+" x "+str(j)+" = "+str(result))
     # TODO create multiplication table function
     # HINT: Use nested for loops (for i in range(1, n + 1): for j in range(1, n + 1):)
     # HINT: Print format should be f"{i} x {j} = {result}"
